7603ff5a2a03745e197982213447247c264f1d78
Shekinah Florance <84413589+flora-shek@users.noreply.github.com>
2024-10-17 07:23:05 +0000

    commit 5-setting up database

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,6 @@
-from application import app
+from application import app,db
 from flask import render_template,request,Response,json
-
+from application.models import User,Course,Enrollment
 courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
 
 @app.route("/api")
@@ -40,5 +40,12 @@ def  enrollment():
     return render_template("enrollment.html",enrollment = True,data={'id':id,'title':title,'term':term})
 
 
+
+@app.route('/user')
+def user():
+    #User(user_id=1,first_name='Lana',last_name='parker',email ='lanaparker@gmail.com',password='kittymeow').save()
+    #User(user_id=2,first_name='Mary',last_name='parker',email ='maryparker@gmail.com',password='kittymeow').save()
+    users =User.objects.all()
+    return render_template("user.html",users=users)
 
 
--- config.py
+++ config.py
@@ -2,4 +2,9 @@ import os
 
 class Config(object):
     SECRET_KEY =  os.environ.get('SECRET_KEY') or "secret_string"
+    MONGODB_SETTINGS ={'db':'UTA_Enrollment',
+                       'username':'florashek24',
+                       'password':'5aujbLlxkX76pbxh',
+                       'host':"mongodb+srv://florashek24:5aujbLlxkX76pbxh@uta-enrollment.7dmnt.mongodb.net/UTA_Enrollment"
+                        }
  
