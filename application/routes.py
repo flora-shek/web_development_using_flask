7aef3c283aa1ae104896d5734d12ac287ed7d59c
Shekinah Florance <84413589+flora-shek@users.noreply.github.com>
2024-11-13 08:36:24 +0000

    Enrollment and registration

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app,db
-from flask import render_template,request,Response,json,flash,redirect,url_for
+from flask import render_template,request,Response,json,flash,redirect,url_for 
 from application.models import User,Course,Enrollment
 from application.forms import LoginForm,RegisterForm
 courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
@@ -31,9 +31,12 @@ def  login():
 
 @app.route("/courses")
 @app.route("/courses/<term>")
-def  courses(term = "2019"):
-
-    return render_template("courses.html" ,courseData=courseData,courses = True,term = term)
+def  courses(term = None):
+    if term is None:
+        term = "Spring 2019"
+    #classes = Course.objects.all()
+    classes = Course.objects.order_by('+courseID')
+    return render_template("courses.html" ,courseData=classes,courses = True,term = term)
 
 @app.route("/register",methods=['POST','GET'])
 def  register():
@@ -51,15 +54,24 @@ def  register():
         user.save()
         flash('You are successfully registered','success')
         return redirect(url_for('index'))
-    return render_template("register.html",register = True)
+    return render_template("register.html",title="Register",form = form,register = True)
 
 @app.route("/enrollment",methods =["GET","POST"])
 def  enrollment():
-    id = request.form.get('courseID')
-    title = request.form.get('title')
+    courseID = request.form.get('courseID')
+    courseTitle = request.form.get('title')
     term = request.form.get('term')
-    
-    return render_template("enrollment.html",enrollment = True,data={'id':id,'title':title,'term':term})
+    user_id =1
+    if courseID:
+        if Enrollment.objects(user_id = user_id,courseID=courseID):
+            flash(f"Oops! You are already registered in the course {courseTitle}!","danger")
+            return redirect(url_for('courses'))
+        else:
+            Enrollment(user_id = user_id,courseID=courseID).save()
+            
+            flash(f"You are enrolled in {courseTitle}!","success")
+    classes = None
+    return render_template("enrollment.html",title = "Enrollemnt",enrollment = True,classes = classes)
 
 
 
